877807797a4b7d07ec456bb6c6375311398102c0
Bength Pappila <bength@typo.local>
2023-02-08 09:31:18 +0100

    no payload?

--- src/main/java/group22/utils/Helpers.java
+++ src/main/java/group22/utils/Helpers.java
@@ -28,7 +28,7 @@ public class Helpers {
         payload = st.toString();
         JSONParser parser = new JSONParser();
         JSONObject json = (JSONObject) parser.parse(payload);
-        System.out.println(json);
+        System.out.println(payload);
         return json;
         
     }
